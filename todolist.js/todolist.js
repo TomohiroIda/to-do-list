e3f40ff4ec687d3c9484ee415b028903508af6cb
Tomohiro Ida <tomohiroida@Tomohiro-no-iMac.local>
2016-07-26 15:36:03 +0900

    fixed some mistakes

--- todolist.js/todolist.js
+++ todolist.js/todolist.js
@@ -1,12 +1,8 @@
 var http = require('http');
 
-http.createSerever(function (request, response){
+http.createServer(function (request, response) {
 response.writeHead(200, {'Content-Type': 'text/plain'});
-respomse.end('Hello World\n');
+response.end('Hello World\n');
 }).listen(8124);
 
-console.log('Server running at http://127.0.0.1:8124/');
-
-~                                                                                                                                                                
-~                                                                                                                                                                
-~                                                     
+console.log('Server running at http://127.0.0.1:8124/');                                                                                                                                                                                                                                                                                                                                                                     
